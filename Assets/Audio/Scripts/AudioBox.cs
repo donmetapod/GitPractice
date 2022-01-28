fd3f137ed829f9a62fc28782cc2af7a82362d610
donmetapod <tyrantmx@gmail.com>
2022-01-28 10:36:39 -0600

    Added resolved conflict characters

--- Assets/Audio/Scripts/AudioBox.cs
+++ Assets/Audio/Scripts/AudioBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AudioBox : MonoBehaviour
 {
-<<<<<<< HEAD
+
     [SerializeField] private AudioSource source;
     
     // Start is called before the first frame update
